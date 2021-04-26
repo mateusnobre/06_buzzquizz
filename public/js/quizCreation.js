2258efa7ae34d9e39c284f4759c0dc466be5437c
mateusnobre <mateusnobrests@gmail.com>
2021-04-26 02:25:27 -0300

    quiz creation payload working

--- public/js/quizCreation.js
+++ public/js/quizCreation.js
@@ -62,78 +62,67 @@ function buildQuestionTemplate(questionNumber) {
     return questionTemplate
 }
 
-
-
-function fillAnswer(question, text, image, isCorrectAnswer) {
+function fillAnswer(text, image, isCorrectAnswer) {
+    var answerJSON = {
+        text: "Texto da resposta 1",
+        image: "https://http.cat/411.jpg",
+        isCorrectAnswer: true
+    }
     answerJSON.text = text;
     answerJSON.image = image;
     answerJSON.isCorrectAnswer = isCorrectAnswer;
-    question.answers.append(answerJSON)
+    return answerJSON
 }
-function fillQuestion(quiz, title, text, color, answers) {
-    questionJSON.title = title;
-    questionJSON.text = text;
-    questionJSON.color = color;
-    for (const answer in answers) {
-        fillAnswer(questionJSON, answer.text, answer.image, answer.isCorrectAnswer);
-    }
-    quiz.questions.append(questionJSON);
+function fillQuestion(title, color, answers) {
+    var questionJSON = {
+        title: title,
+        color: color,
+        answers: answers
+    };
+    quizJSON.questions.push(questionJSON);
 }
-function fillLevel(quiz, title, image, text, minValue) {
-    levelJSON.title = title;
-    levelJSON.image = image;
-    levelJSON.text = text;
-    levelJSON.minValue = minValue;
-    quiz.levels.append(levelJSON)
+function fillLevel(title, image, text, minValue) {
+    var levelJSON = {
+        title: title,
+        image: image,
+        text: text,
+        minValue: minValue
+    };
+    quizJSON.levels.push(levelJSON);
 }
-function fillQuiz(title, image, questions, levels) {
+function fillQuiz(title, image) {
     quizJSON.title = title;
     quizJSON.image = image;
-    for (const question in questions) {
-        fillQuestion(quizJSON, question, title, color, answers);
-    }
-    for (const level in levels) {
-        fillLevel(quiz, title, image, text, minValue);
-    }
-} let quizzTitle = '';
-let quizzImageURL = '';
-let numberOfLevels = 0;
-let numberOfQuestions = 0;
-let answerJSON = {
-    text: "Texto da resposta 1",
-    image: "https://http.cat/411.jpg",
-    isCorrectAnswer: true
-}
-let questionJSON = {
-    title: "Título da pergunta 1",
-    color: "#123456",
-    answers: []
-};
-let levelJSON = {
-    title: "Título do nível 1",
-    image: "https://http.cat/411.jpg",
-    text: "Descrição do nível 1",
-    minValue: 0
-};
-let quizJSON = {
+    console.log(quizJSON);
+} 
+
+var quizzTitle = '';
+var quizzImageURL = '';
+var numberOfLevels = 0;
+var numberOfQuestions = 0;
+
+
+var quizJSON = {
     title: "Título do quizz",
     image: "https://http.cat/411.jpg",
     questions: [],
     levels: []
 }
-let phaseTitles = document.getElementsByClassName('phaseTitle');
-let defineQuizz = document.querySelector('.defineQuizz');
+
+var phaseTitles = document.getElementsByClassName('phaseTitle');
+var defineQuizz = document.querySelector('.defineQuizz');
 defineQuizzInputs = defineQuizz.getElementsByClassName("quizzCreationInput");
-let levelProgressionButtons = document.getElementsByClassName('levelProgressionButton');
-let defineQuestions = document.querySelector('.defineQuestions');
-let defineLevels = document.querySelector('.defineLevels');
-let currentLevel = 1;
-let accessQuizzButton = document.querySelector('.accessQuizzButton');
-let goHomeButton = document.querySelector('.goHomeButton');
-let quizzImage = document.querySelector('.quizzImage');
-let criarQuizz = document.querySelector(".blocoCriarQuizz");
-let quizCreation = document.querySelector(".quizCreation");
-let tela1 = document.querySelector(".tela1");
+var levelProgressionButtons = document.getElementsByClassName('levelProgressionButton');
+var defineQuestions = document.querySelector('.defineQuestions');
+var defineLevels = document.querySelector('.defineLevels');
+var currentLevel = 1;
+var accessQuizzButton = document.querySelector('.accessQuizzButton');
+var goHomeButton = document.querySelector('.goHomeButton');
+var quizzImage = document.querySelector('.quizzImage');
+var blocoCriarQuizz = document.querySelector(".blocoCriarQuizz");
+var seusQuizzes = document.querySelector(".seusQuizzes");
+var quizCreation = document.querySelector(".quizCreation");
+var tela1 = document.querySelector(".tela1");
 
 function phaseProgression() {
     if (currentLevel == 1) {
@@ -145,38 +134,23 @@ function phaseProgression() {
         defineQuizz.classList.add('off');
         quizzTitle = defineQuizzInputs[0].querySelector("input").value;
         quizzImageURL = defineQuizzInputs[1].querySelector("input").value;
+
         numberOfQuestions = defineQuizzInputs[2].querySelector("input").value;
+
         numberOfLevels = defineQuizzInputs[3].querySelector("input").value;
+        if (numberOfQuestions < 3) {
+            currentLevel = 1;
+            alert('Seu quizz deve ter pelo menos 3 perguntas!')
+            window.location.reload();
+        }
+        else if (numberOfLevels < 2) {
+            currentLevel = 1;
+            alert('Seu quizz deve ter pelo menos 2 níveis!')
+            window.location.reload();
+         }
         defineQuestions.classList.remove('off');
         for (let index = 1; index <= numberOfQuestions; index++) {
             defineQuestions.innerHTML += buildQuestionTemplate(index);
-        }
-        questions = defineQuestions.getElementsByClassName('question');
-        for (let index = 0; index < questions.length; index++) {
-            questionInputs = question[i].getElementsByClassName("input")
-            questionTitle = questionInputs[0].value;
-            questionColor = questionInputs[1].value;
-
-            correctAnswerText = questionInputs[2].value;
-            correctAnswerImage = questionInputs[3].value;
-            fillAnswer(questionJSON, correctAnswerText, correctAnswerImage, true);
-
-            let AnswerText = questionInputs[4].value;
-            let AnswerImage = questionInputs[5].value;
-            fillAnswer(questionJSON, AnswerText, AnswerImage, false);
-
-            if (questionInputs.length > 5) {
-                AnswerText = questionInputs[6].value;
-                AnswerImage = questionInputs[7].value;
-                fillAnswer(questionJSON, AnswerText, AnswerImage, false);
-                if (questionInputs.length > 7) {
-                    AnswerText = questionInputs[8].value;
-                    AnswerImage = questionInputs[9].value;
-                    fillAnswer(questionJSON, AnswerText, AnswerImage, false);
-                }
-            }
-            console.log(questionJSON);
-            fillQuestion(quizJSON, questionTitle, questionText, questionColor, answers);
         }
 
     }
@@ -191,9 +165,38 @@ function phaseProgression() {
             defineLevels.innerHTML += buildLevelTemplate(index);
         }
         defineLevels.classList.remove("off");
+        var questionsHTML = defineQuestions.getElementsByClassName('question');
+        var answers = [];
+        for (let index = 0; index < questionsHTML.length; index++) {
+            var questionInputs = questionsHTML[index].getElementsByTagName("input")
+            console.log(questionInputs);
+            var questionTitle = questionInputs[0].value;
+            var questionColor = questionInputs[1].value;
+ 
+            var correctAnswerText = questionInputs[2].value;
+            var correctAnswerImage = questionInputs[3].value;
+            answers.push(fillAnswer(correctAnswerText, correctAnswerImage, true));
+
+            var AnswerText = questionInputs[4].value;
+            var AnswerImage = questionInputs[5].value;
+            answers.push(fillAnswer(AnswerText, AnswerImage, false));
+
+            if (!questionInputs[6] && !questionInputs[7]) {
+                AnswerText = questionInputs[6].value;
+                AnswerImage = questionInputs[7].value;
+                answers.push(fillAnswer(AnswerText, AnswerImage, false));
+                if (!questionInputs[8] && !questionInputs[9]) {
+                    AnswerText = questionInputs[8].value;
+                    AnswerImage = questionInputs[9].value;
+                    answers.push(fillAnswer(AnswerText, AnswerImage, false));
+                }
+            }
+            fillQuestion(questionTitle, questionColor, answers);
+            answers = []
+        }
     }
     else if (currentLevel == 3) {
-        currentLevel += 1;
+        currentLevel = 1;
         phaseTitles[2].classList.add('off');
         phaseTitles[3].classList.remove('off');
         levelProgressionButtons[2].classList.add('off');
@@ -201,6 +204,18 @@ function phaseProgression() {
         accessQuizzButton.classList.remove('off');
         goHomeButton.classList.remove('off');
         quizzImage.classList.remove('off');
+
+        var levels = defineLevels.getElementsByClassName('level');
+        var minValues = [...Array(levels.length).keys()].map(i => i * 100/levels.length);
+        for (let index = 0; index < levels.length; index++) {
+            var levelInputs = levels[index].getElementsByTagName("input");
+            var title = levelInputs[0].value;
+            var image = levelInputs[1].value;
+            var text = levelInputs[2].value;
+            fillLevel(title, image, text, minValues[index]);
+        }
+        fillQuiz(quizzImage, quizzImageURL);
+        quizPost = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/buzzquizz/quizzes', quizJSON)
     }
 }
 
@@ -210,7 +225,9 @@ function createQuizz() {
 }
 
 function sendQuizz() {
+    blocoCriarQuizz.classList.add('off');
     tela1.classList.remove('off');
+    seusQuizzes.classList.remove('off');
     quizCreation.classList.add('off');
-    criarQuizz.classList.add('off');
+    blocoCriarQuizz.style.display = "None";
 }
