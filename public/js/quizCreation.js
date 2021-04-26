d71fe9c3ec82aa500ebc3ef4b2ad908f25bdf95a
mateusnobre <mateusnobrests@gmail.com>
2021-04-25 21:07:22 -0300

    screen transition working

--- public/js/quizCreation.js
+++ public/js/quizCreation.js
@@ -1,7 +1,13 @@
+let questionNumber = 1;
+let level = 1;
+let quizzTitle = '';
+let quizzImageURL = '';
+let numberOfLevels = 0;
+let numberOfQuestions = 0;
 let questionTemplate = `
             <div class="question">
                 <div class="section">
-                    Pergunta 1
+                    Pergunta ${questionNumber}
                 </div>
                 <div class="quizzCreationInput">
                     <input type="text" name="" id="" value="Texto da pergunta">
@@ -39,9 +45,21 @@ let questionTemplate = `
                 <div class="quizzCreationInput">
                     <input type="text" name="" id="" value="URL da imagem 3">
                 </div>
-            </div>
-`;
-let levelTemplate = ``;
+            </div>`;
+let levelTemplate = `<div class="level">
+                        <div class="section">
+                            Nível ${level}
+                        </div>
+                        <div class="quizzCreationInput">
+                            <input type="text" name="" id="" value="Título do nível">
+                        </div>
+                        <div class="quizzCreationInput">
+                            <input type="text" name="" id="" value="URL da imagem do nível">
+                        </div>
+                        <div class="quizzCreationInput">
+                            <input type="text" name="" id="" value="Descrição do nível">
+                        </div>    
+                    </div>`;
 
 let quizTemplate = {
 	title: "Título do quizz",
@@ -114,18 +132,31 @@ let quizTemplate = {
 let phaseTitles = document.getElementsByClassName('phaseTitle');
 let defineQuizz  = document.querySelector('.defineQuizz');
 defineQuizzInputs = defineQuizz.getElementsByClassName("quizzCreationInput");
-let levelProgressionButtons = document.getElementsByClassName('.levelProgressionButton');
+let levelProgressionButtons = document.getElementsByClassName('levelProgressionButton');
 let defineQuestions  = document.querySelector('.defineQuestions');
+let defineLevels = document.querySelector('.defineLevels');
 let currentLevel = 1;
+let accessQuizzButton = document.querySelector('.accessQuizzButton');
+let goHomeButton = document.querySelector('.goHomeButton');
+let quizzImage = document.querySelector('.quizzImage');
 
 function phaseProgression() {
     if (currentLevel == 1){
+        currentLevel += 1;
         phaseTitles[0].classList.add('off');
         phaseTitles[1].classList.remove('off');
         levelProgressionButtons[0].classList.add('off');
         levelProgressionButtons[1].classList.remove('off');
         defineQuizz.classList.add('off');
-        defineQuestions.classList.remove('off');
+        quizzTitle = defineQuizzInputs[0].querySelector("input").value;
+        quizzImageURL = defineQuizzInputs[1].querySelector("input").value;
+        numberOfQuestions = defineQuizzInputs[2].querySelector("input").value;
+        numberOfLevels = defineQuizzInputs[3].querySelector("input").value;
+        for (let index = 0; index < numberOfQuestions; index++) {
+            defineQuestions.innerHTML += questionTemplate;
+            questionNumber += 1;
+        }
+        defineQuestions.classList.remove('off');    
     }
     else if (currentLevel == 2){
         currentLevel +=1;
@@ -134,13 +165,21 @@ function phaseProgression() {
         levelProgressionButtons[1].classList.add('off');
         levelProgressionButtons[2].classList.remove('off');
         defineQuestions.classList.add('off');
+        for (let index = 0; index < numberOfLevels; index++) {
+            defineLevels.innerHTML += levelTemplate;
+            level += 1;
+        }
+        defineLevels.classList.remove("off");
     }
     else if (currentLevel == 3){
         currentLevel +=1;
         phaseTitles[2].classList.add('off');
         phaseTitles[3].classList.remove('off');
         levelProgressionButtons[2].classList.add('off');
-        levelProgressionButtons[3].classList.remove('off');
+        defineLevels.classList.add("off");
+        accessQuizzButton.classList.remove('off');
+        goHomeButton.classList.remove('off');
+        quizzImage.classList.remove('off');
     }
 }
 
